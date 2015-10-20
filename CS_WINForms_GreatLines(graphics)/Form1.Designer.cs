1f127c6145dfba9f31ef8e8eef20ac0d302b0818
FedorVoloshyn <tikey.gm@gmail.com>
2015-10-20 19:07:31 +0300

    commit all

---
+++ CS_WINForms_GreatLines(graphics)/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace CS_WINForms_GreatLines_graphics_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(12, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 474);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Кардиоида",
+            "Строфоида",
+            "Циссоида Диокла",
+            "Декартов Лист",
+            "Верзьера Аньези",
+            "Конхоида Никомеда"});
+            this.comboBox1.Location = new System.Drawing.Point(76, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Tag = "";
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кривая:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(301, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Цвета:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Фон";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Оси";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(519, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "График";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 528);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(846, 567);
+            this.MinimumSize = new System.Drawing.Size(846, 567);
+            this.Name = "Form1";
+            this.Text = "Замечательные кривые";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CS_WINForms_GreatLines(graphics)/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CS_WINForms_GreatLines_graphics_
+{
+    public partial class Form1 : Form
+    {
+        private Pen axesPen, graphPen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            axesPen = new Pen(Color.Black);
+            graphPen = new Pen(Color.Red);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            panel1.BackColor = colorDialog1.Color;
+            panel1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            axesPen.Color = colorDialog1.Color;
+            panel1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            graphPen.Color = colorDialog1.Color;
+            panel1.Invalidate();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics gr = panel1.CreateGraphics();
+            Pen myPen = new Pen(Color.Red);
+            gr.DrawEllipse(myPen, e.X - 1, e.Y - 1, 2, 2);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            double angle;
+            int x = panel1.Width / 2, 
+                y = panel1.Height / 2, 
+                xPrev = 0, 
+                yPrev = 0,
+                a = panel1.Width / 2,
+                b = panel1.Height / 2;
+            Graphics g = panel1.CreateGraphics();
+            g.DrawLine(axesPen, panel1.Width / 2, 0, panel1.Width / 2, panel1.Height);
+            g.DrawLine(axesPen, 0, panel1.Height/2, panel1.Width, panel1.Height/2);
+            int curGraph = comboBox1.SelectedIndex;
+            switch(curGraph)
+            {
+                case 0:
+                    for (angle = 0; angle <= 2 * 3.1416; angle += 0.08) // Кардиоида
+                    {
+                        xPrev = x;
+                        yPrev = y;
+                        x = Convert.ToInt32(a + 100 * (1 - Math.Cos(angle)) * Math.Cos(angle));
+                        y = Convert.ToInt32(b + 100 * (1 - Math.Cos(angle)) * Math.Sin(angle));
+                        g.DrawLine(graphPen, xPrev, yPrev, x, y);
+                        Thread.Sleep(1);
+                    }
+                    break;
+                case 1:
+                    {
+                        int timer = 1;
+                        for (angle = -3.1416 / 2; angle <= 3.1416 / 2; angle += 0.05) // Строфоида
+                        {
+                        A:
+                            xPrev = x;
+                            yPrev = y;
+                            x = Convert.ToInt32(a + 100 * (Math.Tan(angle) * Math.Tan(angle) - 1) / (Math.Tan(angle) * Math.Tan(angle) + 1));
+                            y = Convert.ToInt32(b + 100 * Math.Tan(angle) * ((Math.Tan(angle) * Math.Tan(angle) - 1) / (Math.Tan(angle) * Math.Tan(angle) + 1)));
+                            if (angle == -3.1416 / 2 && --timer == 0)
+                                goto A;
+                            g.DrawLine(graphPen, xPrev, yPrev, x, y);
+                            Thread.Sleep(1);
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        int timer = 1;
+                        for (angle = 0.01; angle <= 3.1416; angle += 0.03) // Циссоида Диокла
+                        {
+                        A:
+                            xPrev = x;
+                            yPrev = y;
+                            x = Convert.ToInt32(a + 100 * 2 / (1 + Math.Tan(angle) * Math.Tan(angle)));
+                            y = Convert.ToInt32(b + 100 * 2 / (Math.Tan(angle) * (1 + Math.Tan(angle) * Math.Tan(angle))));
+                            if (angle == 0.01 && --timer == 0)
+                                goto A;
+                            g.DrawLine(graphPen, xPrev, yPrev, x, y);
+                            Thread.Sleep(1);
+                        }
+                    }
+                    break;
+                case 3:
+                    {
+                        int timer = 1;
+                        for (angle = -3.1416 / 4; angle <= 3.1416 - 3.1416 / 4; angle += 0.03) // Декартов лист
+                        {
+                        A:
+                            xPrev = x;
+                            yPrev = y;
+                            x = Convert.ToInt32(a + 100 * 3 * Math.Tan(angle) / (1 + Math.Tan(angle) * Math.Tan(angle) * Math.Tan(angle)));
+                            y = Convert.ToInt32(b + 100 * 3 * Math.Tan(angle) * Math.Tan(angle) / (1 + Math.Tan(angle) * Math.Tan(angle) * Math.Tan(angle)));
+                            if (angle == -3.1416 / 4 && --timer == 0)
+                                goto A;
+                            g.DrawLine(graphPen, xPrev, yPrev, x, y);
+                            Thread.Sleep(1);
+                        }
+                    }
+                    break;
+
+                case 4:
+                    {
+                        int timer = 1;
+                        for (angle = 0; angle <= 3 * 3.1416; angle += 0.06) // Верзьера Аньези
+                        {
+                        A:
+                            xPrev = x;
+                            yPrev = y;
+                            x = Convert.ToInt32(angle * 100.0);
+                            double k = -a + 100.0 * angle;
+                            y = Convert.ToInt32(b - 50.0 * (8.0 / (4.0 + k * k / 500.0)));
+                            if (angle == 0 && --timer == 0)
+                                goto A;
+                            g.DrawLine(graphPen, xPrev, yPrev, x, y);
+                            Thread.Sleep(1);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    {
+                        int timer = 1;
+                        for (angle = -3.1416 / 2; angle <= 3.1416 + 3.1416 / 2; angle += 0.04) // Конхоида Никомеда
+                        {
+                        A:
+                            xPrev = x;
+                            yPrev = y;
+                            x = Convert.ToInt32(a + 100 * (0.6 * Math.Cos(angle)));
+                            y = Convert.ToInt32(b + 100 * (0.6 * Math.Tan(angle) + 1.2 * Math.Sin(angle)));
+                            if (angle == -3.1416 / 2 && --timer == 0)
+                                goto A;
+                            g.DrawLine(graphPen, xPrev, yPrev, x, y);
+                            Thread.Sleep(1);
+                        }
+                    }
+                    break;
+                default: 
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+        
+    }
+}
